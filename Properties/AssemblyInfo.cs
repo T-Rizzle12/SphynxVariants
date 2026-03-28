25a0926b37426ab8f18a5bcdef0d758a7e838355
Orymizu Smith <isatome022@gmail.com>
2026-03-28 08:46:23 -0400

    Edited

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SphynxVariantsPROTOTYPE")]
+[assembly: AssemblyTitle("SphynxVariants")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SphynxVariantsPROTOTYPE")]
+[assembly: AssemblyProduct("SphynxVariants")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
